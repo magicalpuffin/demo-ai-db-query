004377e14d2623f9ce26f94a92fc97e7d2a4b228
puffins <36088648+magicalpuffin@users.noreply.github.com>
2024-11-27 18:35:31 -0800

    loading db from buffer
    Testing loading db from  buffer, issue with better sqlite and fs

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,144 +1,55 @@
-import { db } from '$lib/server/db';
+// import { db } from '$lib/server/db';
+import { error } from '@sveltejs/kit';
+import Database from 'better-sqlite3';
 import { sql } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
 import type { Actions, PageServerLoad } from './$types';
 
-const DBSCHEMA = `
-CREATE TABLE IF NOT EXISTS "albums"
-(
-    [AlbumId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [Title] NVARCHAR(160)  NOT NULL,
-    [ArtistId] INTEGER  NOT NULL,
-    FOREIGN KEY ([ArtistId]) REFERENCES "artists" ([ArtistId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-CREATE TABLE sqlite_sequence(name,seq);
-CREATE TABLE IF NOT EXISTS "artists"
-(
-    [ArtistId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [Name] NVARCHAR(120)
-);
-CREATE TABLE IF NOT EXISTS "customers"
-(
-    [CustomerId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [FirstName] NVARCHAR(40)  NOT NULL,
-    [LastName] NVARCHAR(20)  NOT NULL,
-    [Company] NVARCHAR(80),
-    [Address] NVARCHAR(70),
-    [City] NVARCHAR(40),
-    [State] NVARCHAR(40),
-    [Country] NVARCHAR(40),
-    [PostalCode] NVARCHAR(10),
-    [Phone] NVARCHAR(24),
-    [Fax] NVARCHAR(24),
-    [Email] NVARCHAR(60)  NOT NULL,
-    [SupportRepId] INTEGER,
-    FOREIGN KEY ([SupportRepId]) REFERENCES "employees" ([EmployeeId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-CREATE TABLE IF NOT EXISTS "employees"
-(
-    [EmployeeId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [LastName] NVARCHAR(20)  NOT NULL,
-    [FirstName] NVARCHAR(20)  NOT NULL,
-    [Title] NVARCHAR(30),
-    [ReportsTo] INTEGER,
-    [BirthDate] DATETIME,
-    [HireDate] DATETIME,
-    [Address] NVARCHAR(70),
-    [City] NVARCHAR(40),
-    [State] NVARCHAR(40),
-    [Country] NVARCHAR(40),
-    [PostalCode] NVARCHAR(10),
-    [Phone] NVARCHAR(24),
-    [Fax] NVARCHAR(24),
-    [Email] NVARCHAR(60),
-    FOREIGN KEY ([ReportsTo]) REFERENCES "employees" ([EmployeeId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-CREATE TABLE IF NOT EXISTS "genres"
-(
-    [GenreId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [Name] NVARCHAR(120)
-);
-CREATE TABLE IF NOT EXISTS "invoices"
-(
-    [InvoiceId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [CustomerId] INTEGER  NOT NULL,
-    [InvoiceDate] DATETIME  NOT NULL,
-    [BillingAddress] NVARCHAR(70),
-    [BillingCity] NVARCHAR(40),
-    [BillingState] NVARCHAR(40),
-    [BillingCountry] NVARCHAR(40),
-    [BillingPostalCode] NVARCHAR(10),
-    [Total] NUMERIC(10,2)  NOT NULL,
-    FOREIGN KEY ([CustomerId]) REFERENCES "customers" ([CustomerId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-CREATE TABLE IF NOT EXISTS "invoice_items"
-(
-    [InvoiceLineId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [InvoiceId] INTEGER  NOT NULL,
-    [TrackId] INTEGER  NOT NULL,
-    [UnitPrice] NUMERIC(10,2)  NOT NULL,
-    [Quantity] INTEGER  NOT NULL,
-    FOREIGN KEY ([InvoiceId]) REFERENCES "invoices" ([InvoiceId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION,
-    FOREIGN KEY ([TrackId]) REFERENCES "tracks" ([TrackId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-CREATE TABLE IF NOT EXISTS "media_types"
-(
-    [MediaTypeId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [Name] NVARCHAR(120)
-);
-CREATE TABLE IF NOT EXISTS "playlists"
-(
-    [PlaylistId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [Name] NVARCHAR(120)
-);
-CREATE TABLE IF NOT EXISTS "playlist_track"
-(
-    [PlaylistId] INTEGER  NOT NULL,
-    [TrackId] INTEGER  NOT NULL,
-    CONSTRAINT [PK_PlaylistTrack] PRIMARY KEY  ([PlaylistId], [TrackId]),
-    FOREIGN KEY ([PlaylistId]) REFERENCES "playlists" ([PlaylistId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION,
-    FOREIGN KEY ([TrackId]) REFERENCES "tracks" ([TrackId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-CREATE TABLE IF NOT EXISTS "tracks"
-(
-    [TrackId] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    [Name] NVARCHAR(200)  NOT NULL,
-    [AlbumId] INTEGER,
-    [MediaTypeId] INTEGER  NOT NULL,
-    [GenreId] INTEGER,
-    [Composer] NVARCHAR(220),
-    [Milliseconds] INTEGER  NOT NULL,
-    [Bytes] INTEGER,
-    [UnitPrice] NUMERIC(10,2)  NOT NULL,
-    FOREIGN KEY ([AlbumId]) REFERENCES "albums" ([AlbumId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION,
-    FOREIGN KEY ([GenreId]) REFERENCES "genres" ([GenreId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION,
-    FOREIGN KEY ([MediaTypeId]) REFERENCES "media_types" ([MediaTypeId]) 
-		ON DELETE NO ACTION ON UPDATE NO ACTION
-);`;
 
-export const load: PageServerLoad = ({ params }) => {
+export const load: PageServerLoad = async ({fetch}) => {
+  const dbfile = await fetch("/chinook.db")
+
+  if (!dbfile.body) return error(500, "Default database missing")
+
+  const chunks = []
+  for await (let chunk of dbfile.body) {
+    chunks.push(chunk)
+  }
+  
+  // const client = new Database(Buffer.concat(chunks), {readonly:true})
+  const client = new Database(":memory:")
+  const db = drizzle(client)
 	const tableSchema = db.all(
 		sql.raw("SELECT * FROM sqlite_master WHERE type='table';")
 	) as object[];
+
 	return {
 		tableSchema
 	};
 };
 
 export const actions = {
-	default: async ({ request, platform }) => {
+	default: async ({ request, platform, fetch }) => {
+    const dbfile = await fetch("/chinook.db")
+    if (!dbfile.body) return error(500, "Default database missing")
+  
+    const chunks = []
+    for await (let chunk of dbfile.body) {
+      chunks.push(chunk)
+    }
+    
+    // const client = new Database(Buffer.concat(chunks), {readonly:true})
+    const client = new Database(":memory:")
+    const db = drizzle(client)
+
+
 		const data = await request.formData();
 		const prompt = data.get('prompt') as string | null;
 		if (!prompt) return { error: 'Prompt is missing' };
+
+    const dbSchema = (db.all(
+      sql.raw("SELECT sql FROM sqlite_master WHERE type='table';")
+    ) as {sql:string}[]).map(item => item.sql).join("")
 
 		const messages = [
 			{
@@ -147,13 +58,14 @@ export const actions = {
 					"You are a model that writes SQL queries. Return a SQL query which meets the user's request.\n" +
 					'Use SQLITE dialect. IMPORTANT: Only return SQL. \n' +
 					'Queries written must be valid for the following database schema: \n' +
-					DBSCHEMA
+					dbSchema
 			},
 			{
 				role: 'user',
 				content: prompt
 			}
 		];
+    // console.log(messages)
 		const response = (await platform?.env.AI.run('@cf/defog/sqlcoder-7b-2', {
 			messages
 		})) as AiTextGenerationOutput;
